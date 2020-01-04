07a1bf573ecde1a891d72d6caf8ed89a7c89464d
marsonfire <awmarsden487@gmail.com>
2020-01-04 16:29:42 -0500

    can add numbers together in a label up top

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import "./index.css";
 
 function Button(props) {
     return (
-        <button>
+        <button onClick={props.onClick}>
             {props.value}
         </button>
+    )
+}
+
+function TextBox(props) {
+    return (
+        <label>
+            {props.value}
+        </label>
     )
 }
 
@@ -15,49 +23,80 @@ class ButtonTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: null,
+            value: 0,
         }
     }
-    putButtonValue(i) {
-        return <Button
+
+    putTextBox(i) {
+        return <TextBox
             value={i}
         />;
     }
 
+    putButtonValue(i) {
+        return <Button
+            value={i}
+            onClick={() => this.buttonClick(i)}
+        />;
+    }
+
+    buttonClick(i) {
+        var num = parseInt(i);
+        if (isNaN(num)) {
+            this.setState({
+                value: "ERROR",
+            })
+        }
+        else {
+            var valueNum = Number(parseInt(this.state.value));
+            if (isNaN(valueNum)) {
+                valueNum = 0;
+            }
+            var setNum = Number(valueNum) + Number(i);
+            this.setState({
+                value: setNum,
+            });
+        }
+    }
+
     render() {
+        let currentTotal = this.state.value;
         return (
             <div>
+                {this.putTextBox(currentTotal)}
                 <table>
-                    <tr>
-                        <td>{this.putButtonValue('P')}</td>
-                        <td>{this.putButtonValue('C')}</td>
-                        <td>{this.putButtonValue('x^2')}</td>
-                        <td>{this.putButtonValue('/')}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.putButtonValue('7')}</td>
-                        <td>{this.putButtonValue('8')}</td>
-                        <td>{this.putButtonValue('9')}</td>
-                        <td>{this.putButtonValue('x')}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.putButtonValue('4')}</td>
-                        <td>{this.putButtonValue('5')}</td>
-                        <td>{this.putButtonValue('6')}</td>
-                        <td>{this.putButtonValue('-')}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.putButtonValue('1')}</td>
-                        <td>{this.putButtonValue('2')}</td>
-                        <td>{this.putButtonValue('3')}</td>
-                        <td>{this.putButtonValue('+')}</td>
-                    </tr>
-                    <tr>
-                        <td>{this.putButtonValue('+/-')}</td>
-                        <td>{this.putButtonValue('0')}</td>
-                        <td>{this.putButtonValue('.')}</td>
-                        <td>{this.putButtonValue('=')}</td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>{this.putButtonValue('P')}</td>
+                            <td>{this.putButtonValue('C')}</td>
+                            <td>{this.putButtonValue('x^2')}</td>
+                            <td>{this.putButtonValue('/')}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.putButtonValue('7')}</td>
+                            <td>{this.putButtonValue('8')}</td>
+                            <td>{this.putButtonValue('9')}</td>
+                            <td>{this.putButtonValue('x')}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.putButtonValue('4')}</td>
+                            <td>{this.putButtonValue('5')}</td>
+                            <td>{this.putButtonValue('6')}</td>
+                            <td>{this.putButtonValue('-')}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.putButtonValue('1')}</td>
+                            <td>{this.putButtonValue('2')}</td>
+                            <td>{this.putButtonValue('3')}</td>
+                            <td>{this.putButtonValue('+')}</td>
+                        </tr>
+                        <tr>
+                            <td>{this.putButtonValue('+/-')}</td>
+                            <td>{this.putButtonValue('0')}</td>
+                            <td>{this.putButtonValue('.')}</td>
+                            <td>{this.putButtonValue('=')}</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         );
