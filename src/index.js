1bdf0b4376c3d76dc78ada9834f5370fced40689
marsonfire <awmarsden487@gmail.com>
2020-01-07 09:30:37 -0500

    working ugly caclulator

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function Button(props) {
     return (
         <button onClick={props.onClick}>
             {props.value}
+            {props.mode}
         </button>
     )
 }
@@ -15,6 +16,7 @@ function TextBox(props) {
     return (
         <label>
             {props.value}
+            {props.op}
         </label>
     )
 }
@@ -24,77 +26,145 @@ class ButtonTable extends React.Component {
         super(props);
         this.state = {
             value: 0,
+            mode: 0,
+            op: '+',
         }
     }
 
-    putTextBox(i) {
+    putTextBox(i, o) {
         return <TextBox
             value={i}
+            op={o}
         />;
     }
 
-    putButtonValue(i) {
+    putNumberButtonValue(i) {
         return <Button
             value={i}
-            onClick={() => this.buttonClick(i)}
+            onClick={() => this.buttonClick(i, this.state.mode)}
         />;
     }
 
-    buttonClick(i) {
+    putOperatorButtonVale(m){
+        return <Button
+            mode={m}
+            onClick={() => this.opButtonClick(m)}
+        />
+    }
+
+    buttonClick(i, m) {
         var num = parseInt(i);
-        if (isNaN(num)) {
+        //if nan, then we're on an operator
+        if(isNaN(num)){
             this.setState({
-                value: "ERROR",
-            })
+                value: "ERROR! SHOULD ONLY BE NUMBERS",
+            });
         }
-        else {
-            var valueNum = Number(parseInt(this.state.value));
-            if (isNaN(valueNum)) {
-                valueNum = 0;
+        else{
+            num = Number(parseFloat(i));
+            if(isNaN(num)){
+                //can happen if we just had an error
+                //easier for when testing too...
+                num = 0; 
             }
-            var setNum = Number(valueNum) + Number(i);
+            else if(m === 0){
+                num = Number(this.state.value) + Number(num);
+                this.setState({
+                    value: num,
+                });
+            }
+            else if(m === 1){
+                num = Number(this.state.value) * Number(num);
+                this.setState({
+                    value: num,
+                });
+            }
+            else if(m === 2){
+                num = Number(this.state.value) - Number(num);
+                this.setState({
+                    value: num,
+                });
+            }
+            else if(m === 3){
+                num = Number(this.state.value) / Number(num);
+                this.setState({
+                    value: num,
+                });
+            }
+        }
+    }
+
+    //m = mode
+    //mode 0 = add
+    //mode 1 = multiply
+    //mode 2 = subtract
+    //mode 3 = divide
+    opButtonClick(m){
+        if(m === '+'){
+            //console.log("setting to addition");
             this.setState({
-                value: setNum,
+                mode: 0,
+                op: m,
+            });
+        }
+        else if(m === 'x'){
+            //console.log("setting to multiplication");
+            this.setState({
+                mode: 1,
+                op: m,
+            });
+        }
+        else if(m === '-'){
+            //console.log("setting to subtraction");
+            this.setState({
+                mode: 2,
+                op: m,
+            });
+        }
+        else if(m === '/'){
+            //console.log("setting to division");
+            this.setState({
+                mode: 3,
+                op: m,
             });
         }
     }
 
     render() {
-        let currentTotal = this.state.value;
         return (
             <div>
-                {this.putTextBox(currentTotal)}
+                {this.putTextBox("Sum: " + this.state.value, "Operator: " + this.state.op)}
                 <table>
                     <tbody>
                         <tr>
-                            <td>{this.putButtonValue('P')}</td>
-                            <td>{this.putButtonValue('C')}</td>
-                            <td>{this.putButtonValue('x^2')}</td>
-                            <td>{this.putButtonValue('/')}</td>
+                            <td>{this.putOperatorButtonVale('P')}</td>
+                            <td>{this.putOperatorButtonVale('C')}</td>
+                            <td>{this.putOperatorButtonVale('x^2')}</td>
+                            <td>{this.putOperatorButtonVale('/')}</td>
                         </tr>
                         <tr>
-                            <td>{this.putButtonValue('7')}</td>
-                            <td>{this.putButtonValue('8')}</td>
-                            <td>{this.putButtonValue('9')}</td>
-                            <td>{this.putButtonValue('x')}</td>
+                            <td>{this.putNumberButtonValue('7')}</td>
+                            <td>{this.putNumberButtonValue('8')}</td>
+                            <td>{this.putNumberButtonValue('9')}</td>
+                            <td>{this.putOperatorButtonVale('x')}</td>
                         </tr>
                         <tr>
-                            <td>{this.putButtonValue('4')}</td>
-                            <td>{this.putButtonValue('5')}</td>
-                            <td>{this.putButtonValue('6')}</td>
-                            <td>{this.putButtonValue('-')}</td>
+                            <td>{this.putNumberButtonValue('4')}</td>
+                            <td>{this.putNumberButtonValue('5')}</td>
+                            <td>{this.putNumberButtonValue('6')}</td>
+                            <td>{this.putOperatorButtonVale('-')}</td>
                         </tr>
                         <tr>
-                            <td>{this.putButtonValue('1')}</td>
-                            <td>{this.putButtonValue('2')}</td>
-                            <td>{this.putButtonValue('3')}</td>
-                            <td>{this.putButtonValue('+')}</td>
+                            <td>{this.putNumberButtonValue('1')}</td>
+                            <td>{this.putNumberButtonValue('2')}</td>
+                            <td>{this.putNumberButtonValue('3')}</td>
+                            <td>{this.putOperatorButtonVale('+')}</td>
                         </tr>
                         <tr>
-                            <td>{this.putButtonValue('+/-')}</td>
-                            <td>{this.putButtonValue('0')}</td>
-                            <td>{this.putButtonValue('.')}</td>
-                            <td>{this.putButtonValue('=')}</td>
+                            <td>{this.putOperatorButtonVale('+/-')}</td>
+                            <td>{this.putNumberButtonValue('0')}</td>
+                            <td>{this.putOperatorButtonVale('.')}</td>
+                            <td>{this.putOperatorButtonVale('=')}</td>
                         </tr>
                     </tbody>
                 </table>
